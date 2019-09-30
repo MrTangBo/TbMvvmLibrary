13ac643f5604c42348e95a1c16e327a420ab2d8e
MrTangBo <609968039@qq.com>
2019-09-30 11:15:34 +0800

    新增ArcView,ArcImageView弧形控件

---
+++ mvvm_library/src/main/java/com/tb/mvvm_library/view/ArcView.kt
@@ -0,0 +1,89 @@
+package com.tb.mvvm_library.view
+
+import android.content.Context
+import android.content.res.TypedArray
+import android.graphics.*
+
+import android.util.AttributeSet
+import android.view.View
+import androidx.core.content.ContextCompat
+import com.tb.mvvm_library.R
+
+/**
+ * @author: MrTangBo
+ * @decribe:
+ */
+class ArcView @JvmOverloads constructor(
+    private val mContext: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : View(mContext, attrs, defStyleAttr) {
+    private var mWidth: Int = 0
+    private var mHeight: Int = 0
+    /**
+     * 弧形高度
+     */
+    private val mArcHeight: Int
+    /**
+     * 背景颜色
+     */
+    private val mBgColor: Int
+
+    private var arcOrientation = "top"
+
+    private val mPaint: Paint
+
+    init {
+        val typedArray = mContext.obtainStyledAttributes(attrs, R.styleable.ArcView)
+        mArcHeight = typedArray.getDimensionPixelSize(R.styleable.ArcView_arcHeight, 0)
+        mBgColor = typedArray.getColor(R.styleable.ArcView_bgColor, Color.parseColor("#303F9F"))
+        arcOrientation = typedArray.getString(R.styleable.ArcView_arcOrientation).toString()
+        typedArray.recycle()
+        mPaint = Paint()
+        mPaint.isDither = true
+        mPaint.isAntiAlias = true
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+
+        mPaint.style = Paint.Style.FILL
+        mPaint.color = mBgColor
+        if (arcOrientation.endsWith("top")) {
+            val rect = Rect(0, 0, mWidth, mHeight)
+            canvas.drawRect(rect, mPaint)
+            mPaint.color = ContextCompat.getColor(mContext, R.color.tb_white)
+            val path = Path()
+            path.moveTo(0f, mHeight.toFloat())
+            path.quadTo((mWidth / 2).toFloat(), (mHeight - mArcHeight).toFloat(), mWidth.toFloat(), mHeight.toFloat())
+            canvas.drawPath(path, mPaint)
+        } else {
+            //矩形部分
+            val rect = Rect(0, 0, mWidth, mHeight - mArcHeight)
+            canvas.drawRect(rect, mPaint)
+            //弧形部分
+            val path = Path()
+            path.moveTo(0f, (mHeight - mArcHeight).toFloat())
+            path.quadTo((mWidth / 2).toFloat(), mHeight.toFloat(), mWidth.toFloat(), (mHeight - mArcHeight).toFloat())
+            canvas.drawPath(path, mPaint)
+        }
+
+
+    }
+
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        val widthSize = View.MeasureSpec.getSize(widthMeasureSpec)
+        val widthMode = View.MeasureSpec.getMode(widthMeasureSpec)
+        val heightSize = View.MeasureSpec.getSize(heightMeasureSpec)
+        val heightMode = View.MeasureSpec.getMode(heightMeasureSpec)
+
+        if (widthMode == View.MeasureSpec.EXACTLY) {
+            mWidth = widthSize
+        }
+        if (heightMode == View.MeasureSpec.EXACTLY) {
+            mHeight = heightSize
+        }
+        setMeasuredDimension(mWidth, mHeight)
+    }
+}
