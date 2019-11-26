48d18eb94e8f947646c0bccc5461f1762696343b
MrTangBo <609968039@qq.com>
2019-11-26 16:33:15 +0800

    2019-11-26 1.新增桌面启动图标的切换 2.新增创建桌面快捷图标的创建，全兼容（随着版本的不同有差异）

--- mvvm_library/src/main/java/com/tb/mvvm_library/tbExtend/TbContextExtend.kt
+++ mvvm_library/src/main/java/com/tb/mvvm_library/tbExtend/TbContextExtend.kt
@@ -1,8 +1,18 @@
 package com.tb.mvvm_library.tbExtend
 
+import android.annotation.SuppressLint
 import android.app.Activity
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.content.pm.ShortcutInfo
+import android.content.pm.ShortcutManager
+import android.graphics.drawable.Icon
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.util.DisplayMetrics
@@ -11,6 +21,9 @@ import android.view.WindowManager
 import android.view.inputmethod.InputMethodManager
 import androidx.core.app.ActivityOptionsCompat
 import androidx.core.content.ContextCompat
+import androidx.core.content.pm.ShortcutInfoCompat
+import androidx.core.content.pm.ShortcutManagerCompat
+import androidx.core.graphics.drawable.IconCompat
 import androidx.fragment.app.Fragment
 import com.tb.mvvm_library.R
 import com.tb.mvvm_library.base.TbApplication
@@ -18,8 +31,10 @@ import com.tb.mvvm_library.hipermission.HiPermission
 import com.tb.mvvm_library.hipermission.PermissionCallback
 import com.tb.mvvm_library.hipermission.PermissionItem
 import com.tb.mvvm_library.tbEntity.TbApkInfo
+import com.tb.mvvm_library.util.LogUtils
 import com.tb.mvvm_library.util.SystemBarUtil
 import java.io.*
+
 
 /**
  *@作者：tb
@@ -244,7 +259,122 @@ fun Any.tbApkInfo(): TbApkInfo {
     return apkInfo
 }
 
+/*修改桌面图标，只针对启动图标*/
+fun Activity.tbChangeDeskIcon(showActivityName: String, hideActivityName: String) {
+    val show = ComponentName(this, "${packageName}.$showActivityName")
+    val hide = ComponentName(this, "${packageName}.$hideActivityName")
+    if (packageManager.getComponentEnabledSetting(show) == PackageManager.COMPONENT_ENABLED_STATE_DISABLED) {
+        packageManager.setComponentEnabledSetting(
+            show,
+            PackageManager.COMPONENT_ENABLED_STATE_ENABLED,
+            PackageManager.DONT_KILL_APP
+        )
+    }
+    if (packageManager.getComponentEnabledSetting(hide) == PackageManager.COMPONENT_ENABLED_STATE_ENABLED) {
+        packageManager.setComponentEnabledSetting(
+            hide,
+            PackageManager.COMPONENT_ENABLED_STATE_DISABLED,
+            PackageManager.DONT_KILL_APP
+        )
+    }
 
+}
+
+/*创建Activity桌面快捷图标*/
+fun Activity.tbCreateDeskIcon(name: String, icon: Int) {
+    if (!ShortcutManagerCompat.isRequestPinShortcutSupported(this)) {
+        tbShowToast("该设备不支持创建快捷方式！")
+        return
+    }
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        var isCreate = false
+        val intent = Intent(this, javaClass)
+        intent.action = Intent.ACTION_VIEW
+        val shortcutInfo = ShortcutInfoCompat.Builder(this, name)
+            .setIcon(IconCompat.createWithResource(this, icon))
+            .setIntent(intent)
+            .setShortLabel(name)
+            .build()
+        val list = ShortcutManagerCompat.getDynamicShortcuts(this)
+        LogUtils.log("shortcutInfos--->${list.size}")
+        list.forEach {
+            if (it.shortLabel == shortcutInfo.shortLabel) {
+                isCreate = true
+            }
+        }
+        if (isCreate) {
+            tbShowToast("$name 快捷方式已存在！")
+        } else {
+            if (list.size > 4) {
+                tbShowToast("最多支持4个快捷方式")
+                return
+            }
+            if (Build.VERSION.SDK_INT == Build.VERSION_CODES.O || Build.VERSION.SDK_INT == Build.VERSION_CODES.O_MR1) {
+                ShortcutManagerCompat.addDynamicShortcuts(this, arrayListOf(shortcutInfo))
+            } else {
+                val peddingIntent =
+                    PendingIntent.getBroadcast(
+                        this,
+                        0,
+                        Intent(this, BroadcastReceiver::class.java),
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                    )
+                ShortcutManagerCompat.addDynamicShortcuts(this, arrayListOf(shortcutInfo))
+                ShortcutManagerCompat.requestPinShortcut(this, shortcutInfo, peddingIntent.intentSender)
+                tbShowToast("添加成功！")
+            }
+        }
+    } else {
+        if (tbCheckShortCutExist(this, name)) {
+            tbShowToast("$name 快捷方式已存在！")
+            return
+        }
+        //创建快捷方式的intent广播
+        val intent = Intent("com.android.launcher.action.INSTALL_SHORTCUT")
+        //添加快捷名称
+        intent.putExtra(Intent.EXTRA_SHORTCUT_NAME, name)
+        //  快捷图标不允许重复
+        intent.putExtra("duplicate", true)
+        // 快捷图标
+        val iconRes = Intent.ShortcutIconResource.fromContext(this, icon)
+        intent.putExtra(Intent.EXTRA_SHORTCUT_ICON_RESOURCE, iconRes)
+        //我们下次启动要用的Intent信息
+        val carryIntent = Intent()
+        carryIntent.setClass(this, javaClass)
+        carryIntent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+        //添加携带的Intent
+        intent.putExtra(Intent.EXTRA_SHORTCUT_INTENT, carryIntent)
+        //  发送广播
+        sendBroadcast(intent)
+        tbShowToast("添加成功！")
+    }
+}
+
+
+/*判断快捷方式是否存在*/
+@SuppressLint("ObsoleteSdkInt")
+fun tbCheckShortCutExist(mContent: Context, name: String): Boolean {
+    var isInstallShortcut = false
+    val authority: String = when {
+        Build.VERSION.SDK_INT < 8 -> "content://com.android.launcher.settings/favorites?notify=true"
+        Build.VERSION.SDK_INT < 19 -> "content://com.android.launcher2.settings/favorites?notify=true"
+        else -> "content://com.android.launcher3.settings/favorites?notify=true"
+    }
+    val cr = mContent.contentResolver
+    val uri = Uri.parse(authority)
+    try {
+        val cursor = cr.query(uri, arrayOf("title", "iconResource"), "title=?", arrayOf(name), null)
+        if (cursor != null && cursor.count > 0) {
+            isInstallShortcut = true
+        }
+        if (null != cursor && !cursor.isClosed)
+            cursor.close()
+
+    } catch (e: Exception) {
+        LogUtils.log("e--->$e")
+    }
+    return isInstallShortcut
+}
 
 
 
--- mvvm_library/src/main/java/com/tb/mvvm_library/tbExtend/tbSystemExtend.kt
+++ mvvm_library/src/main/java/com/tb/mvvm_library/tbExtend/tbSystemExtend.kt
@@ -1,7 +1,10 @@
 package com.tb.mvvm_library.tbExtend
 
+import android.app.ActivityManager
 import android.app.NotificationManager
+import android.content.Context.ACTIVITY_SERVICE
 import android.content.Context.NOTIFICATION_SERVICE
+import android.content.pm.PackageManager
 import com.tb.mvvm_library.base.TbApplication
 
 /**
@@ -14,4 +17,6 @@ import com.tb.mvvm_library.base.TbApplication
 val Any.tbNotificationManager: NotificationManager
     get() = TbApplication.mApplicationContext.getSystemService(NOTIFICATION_SERVICE) as NotificationManager
 
-
+/*获取ActivityManager*/
+val Any.tbActivityManager: ActivityManager
+    get() = TbApplication.mApplicationContext.getSystemService(ACTIVITY_SERVICE) as ActivityManager
